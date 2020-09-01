d569cbef8a31abbb959424c4481f91638a0d9664
James Hortle <jamesohortle@gmail.com>
2020-09-02 01:00:04 +1000

    [fix] TripCount class's data types; [fix] TCController's query and output.

--- NYCCab/src/main/java/com/example/NYCCab/TripCount.java
+++ NYCCab/src/main/java/com/example/NYCCab/TripCount.java
@@ -1,21 +1,21 @@
 package com.example.NYCCab;
 
 public class TripCount {
-	private final long medallion;
-	private final long date;
+	private final String medallion;
+	private final String date;
 	private final long numTrips;
 	
-	public TripCount(long medallion, long date, long numTrips) {
+	public TripCount(String medallion, String date, long numTrips) {
 		this.medallion = medallion;
 		this.date = date;
 		this.numTrips = numTrips;
 	}
 	
-	public long getMedallion() {
+	public String getMedallion() {
 		return medallion;
 	}
 	
-	public long getDate() {
+	public String getDate() {
 		return date;
 	}
 	
