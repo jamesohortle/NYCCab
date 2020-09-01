f13162c238fec3375b70677bcff5d90e137b6ec0
James Hortle <jamesohortle@gmail.com>
2020-09-01 17:13:18 +1000

    [add] initialize with Spring Initializr.

---
+++ NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
@@ -0,0 +1,13 @@
+package com.example.NYCCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NycCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NycCabApplication.class, args);
+	}
+
+}
