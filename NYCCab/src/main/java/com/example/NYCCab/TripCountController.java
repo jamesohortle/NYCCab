1ee7211198fe99c7bcfe4ef1571a13bd3d1bd60f
James Hortle <jamesohortle@gmail.com>
2020-09-02 02:00:34 +1000

    [fix] refactor to return an ArrayList of TripCounts

--- NYCCab/src/main/java/com/example/NYCCab/TripCountController.java
+++ NYCCab/src/main/java/com/example/NYCCab/TripCountController.java
@@ -1,11 +1,14 @@
 package com.example.NYCCab;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -13,60 +16,85 @@ import java.util.regex.Matcher;
 public class TripCountController {
 	
 	@GetMapping("/tripCount")
-	public TripCount tripCount(
-			@RequestParam(value = "medallion", required = true) String medallion,
+	public ArrayList<TripCount> tripCount(
+			@RequestParam(value = "medallion", required = true) String[] medallions,
 			@RequestParam(value = "date", required = false) String date
-	) {
-		// Validate medallion.
-		// Medallion is a hexadecimal value with 32 digits.
+	) throws Exception {
+		// Validate medallions.
+		// A medallion is a hexadecimal value with 32 digits.
 		// Therefore, in range 0 to 0xFFF...FFF (32 long).
 		// Split in half since 0xF...F (16 long) <= 2^64 -1 = MAX_INT 
 		// Try parsing first to see if valid.
-		assert(medallion.length() == 32);
-		try {
-			Long.parseUnsignedLong(medallion.substring(0, 16), 16);
-			Long.parseUnsignedLong(medallion.substring(16), 16);	
-		} catch (NumberFormatException ex) {
-			ex.printStackTrace();
-			return new TripCount("", "", 0L);
+		ArrayList<String> validMedallions = new ArrayList<String>();
+		for (String medallion : medallions) {
+			assert(medallion.length() == 32);
+			try {
+				if (medallion.length() != 32) {
+					throw new MedallionException("Medallion wrong length.");
+				}
+				Long.parseUnsignedLong(medallion.substring(0, 16), 16);
+				Long.parseUnsignedLong(medallion.substring(16), 16);
+				
+				// Add to validMedallions.
+				validMedallions.add(medallion);
+			} catch (NumberFormatException ex) {
+				continue;
+			}catch (MedallionException ex) {
+				continue;
+			}
+		}
+		
+		System.out.println("Valid medallions are:");
+		for (String medallion : validMedallions) {
+			System.out.println(medallion);
 		}
 		
 		// Validate date.
 		// For now, accept only yyyy-mm-dd, like database.
-		assert(date.length() == 10);
+		// Accept only one date.
+		System.out.println(date);
 		Pattern datePattern = Pattern.compile("\\d{4}-[01]\\d-[0-3]\\d");
 		Matcher dateMatch = datePattern.matcher(date);
 		if (!dateMatch.matches()) {
-			return new TripCount("", "", 0L);
+			throw new Exception("Not a valid date.");
 		}
 		
+		System.out.println(String.format("Date to query: %s.", date));
 		
-		// Query database.
+		
+		// Query database and append counts for each medallion.
+		// If medallion not found, count = 0.
 		String url = "jdbc:mysql://localhost:3306/***REMOVED***?serverTimezone=Australia/Sydney&useLegacyDatetimeCode=false";
 		String username = "***REMOVED***";
 		String password = "***REMOVED******REMOVED***";
 		
 		System.out.println("Connecting database...");
-		
-		try (Connection connection = DriverManager.getConnection(url, username, password)) {
-		    System.out.println("Database connected!");
-		    Statement stmt = connection.createStatement();
+		ArrayList<TripCount> tc = new ArrayList<TripCount>();
+		for (String medallion : medallions) {
 			String query = String.format(
 					"SELECT COUNT(*) FROM cab_trip_data WHERE medallion = '%s' AND DATE(pickup_datetime) = '%s';",
 					medallion,
 					date);
-			ResultSet result = stmt.executeQuery(query);
-			if (result.next()) {
-				long count = result.getInt("COUNT(*)");
-				return new TripCount(medallion, date, count);
-			} else {
-				
+			try (
+				// conn, stmt, result will autoclose.
+				Connection conn = DriverManager.getConnection(url, username, password);
+				Statement stmt = conn.createStatement();
+				ResultSet result = stmt.executeQuery(query);
+					
+			) {
+				System.out.println("Database connected!");
+				if (result.next()) {
+					long count = result.getInt("COUNT(*)");
+					tc.add(new TripCount(medallion, date, count));
+				} else {
+					tc.add(new TripCount(medallion, date, 0L));
+				}
+			} catch (SQLException e) {
+			    throw new IllegalStateException("Cannot connect to database!", e);
 			}
-		} catch (SQLException e) {
-		    throw new IllegalStateException("Cannot connect the database!", e);
 		}
 		
 		
-		return new TripCount("", "", 0L);
+		return tc;
 	}
 }
