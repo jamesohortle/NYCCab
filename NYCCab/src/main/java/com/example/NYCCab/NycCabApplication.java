de3ae072eae9ed33d5aca0bad8c3daeb22fa4427
James Hortle <jamesohortle@gmail.com>
2020-09-02 17:28:48 +1000

    [fix] use encrypted application properties and not raw strings

--- NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
+++ NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
@@ -1,10 +1,9 @@
 package com.example.NYCCab;
 
+
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 
 @SpringBootApplication
 @EnableEncryptableProperties
