d030026e73bea2aaa230681571cdc95afd32593b
James Hortle <jamesohortle@gmail.com>
2020-09-02 16:35:01 +1000

    [add] enable encryption

--- NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
+++ NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
@@ -1,9 +1,13 @@
 package com.example.NYCCab;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class NycCabApplication {
 
 	public static void main(String[] args) {
