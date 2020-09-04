ca99a9dbfcf42998dbf10cd46ba702d3549ca40f
James Hortle <jamesohortle@gmail.com>
2020-09-05 01:06:33 +1000

    [fix] enable caching for Spring Boot Application

--- NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
+++ NYCCab/src/main/java/com/example/NYCCab/NycCabApplication.java
@@ -1,10 +1,11 @@
 package com.example.NYCCab;
 
-
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 @EnableEncryptableProperties
 public class NycCabApplication {
